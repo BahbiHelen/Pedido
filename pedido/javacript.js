90e528bdc0aadde3833b8621b4ad84633b580604
Bahbi <133366088+BahbiHelen@users.noreply.github.com>
2024-04-18 11:04:26 -0300

    Add files via upload

---
+++ pedido/javacript.js
@@ -0,0 +1,5 @@
+document.getElementById('sim-btn').addEventListener('click', function() {
+    window.location.href = 'https://open.spotify.com/intl-pt/track/3EBtHRt4uSSFhNladqH3UP?si=349a299aa6554ca7'; // Substitua 'link_para_sua_musica.mp3' pelo link da sua música
+  });
+
+  
